cd5a1d5d1b4b72ff7849bd912dac205a0c67a315
PraveenAnilKumar <aspraveen67@gmail.com>
2025-01-12 19:13:59 +0530

    Update PrimsAlgorithm.c

--- PrimsAlgorithm.c
+++ PrimsAlgorithm.c
@@ -1,73 +1,70 @@
 #include <stdio.h>
 #define MAX 10
-int main()
-{
+#define INF 999
+
+int main() {
     int vertex_array[MAX], counter;
     int vertex_count = 0;
     int row, column;
     int cost_matrix[MAX][MAX];
     int visited[MAX] = {0};
     int edge_count = 0, count = 1;
-    int sum_cost = 0, min_cost = 0;
-    int row_no, column_no, vertex1, vertex2;
-    printf("Total no of vertex :: ");
+    int sum_cost = 0, min_cost;
+    int row_no = 0, column_no = 0, vertex1, vertex2;
+
+    printf("Total no of vertices: ");
     scanf("%d", &vertex_count);
-    printf("\n-- Enter vertex -- \n\n");
-    for (counter = 1; counter <= vertex_count; counter++)
-    {
-        printf("vertex[%d] :: ", counter);
+
+    if (vertex_count > MAX || vertex_count <= 0) {
+        printf("Invalid number of vertices. Exiting.\n");
+        return 1;
+    }
+
+    printf("\n-- Enter vertex labels --\n");
+    for (counter = 0; counter < vertex_count; counter++) {
+        printf("vertex[%d]: ", counter + 1);
         scanf("%d", &vertex_array[counter]);
     }
-    printf("\n--- Enter Cost matrix of size %d x %d ---\n\n", vertex_count, vertex_count);
-    printf("\n\t-- format is --\n");
-    for (row = 1; row <= vertex_count; row++)
-    {
-        for (column = 1; column <= vertex_count; column++)
-        {
-            printf("x ");
-        }
-        printf("\n");
-    }
-    printf("\n-- MATRIX --\n\n");
-    for (row = 1; row <= vertex_count; row++)
-    {
-        for (column = 1; column <= vertex_count; column++)
-        {
+
+    printf("\n--- Enter Cost Matrix (%d x %d) ---\n", vertex_count, vertex_count);
+    for (row = 0; row < vertex_count; row++) {
+        for (column = 0; column < vertex_count; column++) {
+            printf("Cost[%d][%d]: ", row + 1, column + 1);
             scanf("%d", &cost_matrix[row][column]);
-            if (cost_matrix[row][column] == 0)
-            {
-                cost_matrix[row][column] = 999;
+            if (cost_matrix[row][column] == 0 && row != column) {
+                cost_matrix[row][column] = INF; // Replace 0 with INF for non-self-loops
             }
         }
     }
-    printf("\n");
-    visited[1] = 1;
+
+    visited[0] = 1; // Start from the first vertex
     edge_count = vertex_count - 1;
-    while (count <= edge_count)
-    {
-        for (row = 1, min_cost = 999; row <= vertex_count; row++)
-        {
-            for (column = 1; column <= vertex_count; column++)
-            {
-                if (cost_matrix[row][column] < min_cost)
-                {
-                    if (visited[row] != 0)
-                    {
+
+    printf("\nEdges in the Minimum Spanning Tree:\n");
+    while (count <= edge_count) {
+        min_cost = INF;
+
+        for (row = 0; row < vertex_count; row++) {
+            if (visited[row]) {
+                for (column = 0; column < vertex_count; column++) {
+                    if (!visited[column] && cost_matrix[row][column] < min_cost) {
                         min_cost = cost_matrix[row][column];
-                        vertex1 = row_no = row;
-                        vertex2 = column_no = column;
+                        vertex1 = row;
+                        vertex2 = column;
                     }
                 }
             }
         }
-        if (visited[row_no] == 0 || visited[column_no] == 0)
-        {
-            printf("\nEdge %d is (%d -> %d)with cost:%d", count++, vertex_array[vertex1], vertex_array[vertex2], min_cost);
-            sum_cost = sum_cost + min_cost;
-            visited[column_no] = 1;
+
+        if (!visited[vertex2]) {
+            printf("Edge %d: (%d -> %d) with cost: %d\n", count++, vertex_array[vertex1], vertex_array[vertex2], min_cost);
+            sum_cost += min_cost;
+            visited[vertex2] = 1;
         }
-        cost_matrix[vertex1][vertex2] = cost_matrix[vertex2][vertex1] = 999;
+
+        cost_matrix[vertex1][vertex2] = cost_matrix[vertex2][vertex1] = INF; // Mark edge as used
     }
-    printf("\n\nMinimum cost=%d", sum_cost);
+
+    printf("\nMinimum cost of the spanning tree: %d\n", sum_cost);
     return 0;
 }
